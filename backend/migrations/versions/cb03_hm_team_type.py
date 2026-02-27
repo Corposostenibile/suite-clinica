95619b8b8beb001f39a1891e6e964eda40beecc4
Emanuele Mastronardi <emanuele.mastronardi.2002.12@gmail.com>
2026-02-27 11:56:55 +0100

    Shorten Alembic revision id for HM team type migration

--- backend/migrations/versions/cb03_hm_team_type.py
+++ backend/migrations/versions/cb03_hm_team_type.py
@@ -1,6 +1,6 @@
 """add_health_manager_team_type
 
-Revision ID: cb03_add_health_manager_team_type
+Revision ID: cb03_hm_team_type
 Revises: cb02_durata_servizio_giorni
 Create Date: 2026-02-27
 
@@ -8,7 +8,7 @@ Create Date: 2026-02-27
 from alembic import op
 
 
-revision = "cb03_add_health_manager_team_type"
+revision = "cb03_hm_team_type"
 down_revision = "cb02_durata_servizio_giorni"
 branch_labels = None
 depends_on = None
@@ -21,4 +21,3 @@ def upgrade():
 def downgrade():
     # PostgreSQL non supporta la rimozione diretta di valori enum senza ricreare il tipo.
     pass
-
